62b0257c24c9878b7fa259b86b509be4d3e154c8
Vinícius Garcia <vingarcia00@gmail.com>
2022-10-11 23:41:31 -0300

    Simplify foo package

--- foo.go
+++ foo.go
@@ -2,20 +2,12 @@ package foo
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/vingarcia/bar"
 )
 
 func Main() {
-	port := "1358"
-	fmt.Printf("Starting server on port %s...\n", port)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "VinGarcia says Oi Caio!!")
-		fmt.Printf("received request: %#v\n", r)
-	})
+	fmt.Printf("In foo package rigit now\n")
 
-	fmt.Println(bar.Bar{})
-
-	http.ListenAndServe(":"+port, nil)
+	fmt.Println("printing a Bar struct:", bar.Bar{})
 }
